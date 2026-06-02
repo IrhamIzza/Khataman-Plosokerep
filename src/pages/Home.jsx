a38a20ca418c86125b5e2acea79d5c0800d4bab9
IrhamIzza <Irhamizza21@gmail.com>
2026-06-02 10:24:36 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import { useMemo } from "react";
+import { useMemo, useState, useRef } from "react";
+import foto1 from "../assets/a.jpg";
+import foto2 from "../assets/b.jpg";
+import foto3 from "../assets/c.jpg";
+import { toPng } from "html-to-image";
 
 export default function Home() {
+  const [tempat, setTempat] = useState(
+    "Bpk. Anas Dhofir Jln. Ploso No.... RT.02 RW.02 Kota Blitar",
+  );
+
   const now = new Date();
+  // const now = new Date("2026-06-01");
 
   const nama = [
     "Anggota - Sjakur",
@@ -50,11 +59,11 @@ export default function Home() {
   }));
 
   const formatTanggal = (date) => {
-    const hari = String(date.getDate()).padStart(2, "0");
-    const bulan = String(date.getMonth() + 1).padStart(2, "0");
-    const tahun = date.getFullYear();
-
-    return `${hari}-${bulan}-${tahun}`;
+    return date.toLocaleDateString("id-ID", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
   };
 
   const nextMalamMinggu = () => {
@@ -73,10 +82,83 @@ export default function Home() {
     return formatTanggal(next);
   };
 
+  const divRef = useRef(null);
+
+  const downloadImage = async () => {
+    if (!divRef.current) return;
+
+    const dataUrl = await toPng(divRef.current, {
+      quality: 1,
+      pixelRatio: 2,
+      cacheBust: true,
+      backgroundColor: "#ffffff",
+    });
+
+    const link = document.createElement("a");
+    link.download = "undangan-khataman.png";
+    link.href = dataUrl;
+    link.click();
+  };
+
   return (
     <div className="container mx-auto">
+      <div className="mb-2 p-4">
+        <label htmlFor="tempat" className="block mb-1 font-medium">
+          Tempat Khataman
+        </label>
+
+        <textarea
+          id="tempat"
+          value={tempat}
+          onChange={(e) => setTempat(e.target.value)}
+          rows={2}
+          className="w-full border p-2"
+        />
+      </div>
+
+      <div ref={divRef} className="bg-white p-4">
+        <div className="flex p-4 ">
+          <div className="flex flex-col mx-auto">
+            <img src={foto1} alt="logo" className="mx-auto w-22 h-22" />
+            <img src={foto2} alt="logo" className="mx-auto h-30" />
+
+            <div className="text-lg">
+              <p>
+                Ngaturi pirso dumateng sedoyo anggota Jam’iyah Khotmil Qur’an,
+                bilih mbenjing dinten{" "}
+                <span className="font-bold text-blue-800 underline">
+                  Sabtu malem Ahad tgl 06 Juni 2026, ba'dah Maghrib
+                </span>{" "}
+                awal wonten dalem ipun{" "}
+                <span className="font-bold text-blue-800 underline whitespace-pre-line">
+                  {tempat}
+                </span>
+                .
+              </p>
+              <p>Makaten saking kulo, matur nuwun🙏. </p>
+            </div>
+
+            <img src={foto3} alt="logo" className="h-26" />
+            <div className="text-lg text-right">
+              <p className="mr-5">TTD</p>
+              <div className="h-10"></div>
+              <p>Pengurus</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          onClick={downloadImage}
+          className="px-4 py-2 bg-blue-600 text-white rounded cursor-pointer"
+        >
+          Download Gambar
+        </button>
+      </div>
+
       <div className="text-center font-bold text-xl mt-4">Khataman</div>
-      
+
       <div className="flex flex-col p-4">
         <div className="flex">
           <div className="flex-1">Hari ini</div>
@@ -97,7 +179,7 @@ export default function Home() {
         </div>
 
         {dataKhataman.map((item, index) => (
-          <div className="grid grid-cols-[50px_1fr_60px]">
+          <div key={index} className="grid grid-cols-[50px_1fr_60px]">
             <div className="border p-2 text-center">{index + 1}</div>
 
             <div className="border p-2">{item.nama}</div>
